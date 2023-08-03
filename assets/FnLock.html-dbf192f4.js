2d2218c465ee37d06f7f67b28af1c0c065876b54
Tstar00 <6105121024@email.ncu.edu.cn>
2023-08-03 07:31:53 +0000

    Deploying to gh-pages from @ Tstar00/Tstar00.github.io@67ea0acd3d2469af0d4eb7e15f793e6cad33fc4e 🚀

---
+++ assets/FnLock.html-dbf192f4.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./plugin-vue_export-helper-c27b6911.js";import{o as t,c,d as e,e as n}from"./app-3b80d1a3.js";const s={},r=e("p",null,[n("部分笔记本键盘按热键无效的话，请检查是否有"),e("strong",null,"Fn锁定"),n("，一般按 Fn+ESC/空格 可以解除锁定，或者按 Fn+F2，Fn+F3 来使用热键")],-1),_=[r];function a(l,d){return t(),c("div",null,_)}const m=o(s,[["render",a],["__file","FnLock.html.vue"]]);export{m as default};
