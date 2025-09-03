90821643f2f804fee01cd6efd555e826db440be5
Tstar00 <6105121024@email.ncu.edu.cn>
2025-09-03 14:50:13 +0000

    Deploying to gh-pages from @ soku-cn/soku-cn.github.io@85bd847f0cf1071f77bd192d74aeafeb8ee36e78 🚀

---
+++ assets/Blurred.html-C9nHge9x.js
@@ -0,0 +1,33 @@
+import{_ as e,o as i,c as n,e as t}from"./app-CLfWUKoC.js";const s={},d=t(`<div class="hint-container info"><p class="hint-container-title">相关信息</p><ul><li>按 Alt + Enter 切换全屏</li></ul><p>1、 <strong>请检查 <code>[修复]【 重置游戏窗口的位置和大小 】.bat</code> 或 <code>th123\\modules\\WindowResizer\\WindowResizer.ini</code> ,建议使用 一倍 两倍 三倍 的整数倍窗口缩放，而不要手动拖拉窗口，否则不规则倍数缩放可能会导致游戏内文字或图片显示模糊等问题</strong></p><p>2、 可以试试右键 <code>th123.exe</code>，选择属性，兼容性，勾选 “高DPI缩放替代”，选择 “应用程序”<br><strong>不要选择“系统(增强)”，会导致游戏打不开</strong></p><figure><img src="https://bu.dusays.com/2024/10/26/671cac0b857ea.webp" alt="可以试试勾选“高DPI缩放替代”，选择“应用程序”" width="900" tabindex="0" loading="lazy"><figcaption>可以试试勾选“高DPI缩放替代”，选择“应用程序”</figcaption></figure><p>3、 游戏本身就是 640*480 的分辨率，游戏本身美术素材就是像素画 ㄟ( ▔, ▔ )ㄏ<br> 同时也可能你的显示屏太大，放大倍数过大当然就糊了</p></div><details class="hint-container details"><summary>WindowResizer-back-2x.ini</summary><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>
+; https://wiki.514.live/FAQ/In-Games/Blurred.html
+; 感觉游戏画面模糊
+
+[Size]
+; 设置为 1 允许调整窗口和自由拖拉窗口（保持比例），设置为 0 不允许调整和拖拉窗口
+; 建议使用固定的整数倍窗口缩放，而不要手动拖拉窗口，否则不规则倍数缩放可能会导致游戏内文字或图片显示模糊等问题
+Enabled=1
+
+; 一倍窗口 1x
+; Width=640
+; Height=480
+
+; 两倍窗口 2x
+Width=1280
+Height=960
+
+; 三倍窗口 3x
+; Width=1920
+; Height=1440
+
+
+[Position]
+; 设置为 1 表示记忆游戏上次运行时的位置，设置为 0 表示不记忆
+Enabled=1
+X=1
+Y=1
+
+[Bars]
+; 设置为 1 表示全屏（无边框全屏，非独占全屏）时两边添加黑边，设置为 0 表示不添加黑边（但是依然是 4:3 比例，不会铺满屏幕）
+Enabled=1
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div></details>`,2),a=[d];function r(l,c){return i(),n("div",null,a)}const m=e(s,[["render",r],["__file","Blurred.html.vue"]]),u=JSON.parse('{"path":"/FAQ/In-Games/Blurred.html","title":"感觉游戏画面模糊","lang":"zh-CN","frontmatter":{"title":"感觉游戏画面模糊","icon":"circle-info","order":7,"editLink":false,"author":"三回転Tstar","date":"2024-07-09T00:00:00.000Z","description":"相关信息 按 Alt + Enter 切换全屏 1、 请检查 [修复]【 重置游戏窗口的位置和大小 】.bat 或 th123\\\\modules\\\\WindowResizer\\\\WindowResizer.ini ,建议使用 一倍 两倍 三倍 的整数倍窗口缩放，而不要手动拖拉窗口，否则不规则倍数缩放可能会导致游戏内文字或图片显示模糊等问题 2、 可以试试右键...","head":[["meta",{"property":"og:url","content":"https://wiki.514.live/FAQ/In-Games/Blurred.html"}],["meta",{"property":"og:site_name","content":"非想天则指南"}],["meta",{"property":"og:title","content":"感觉游戏画面模糊"}],["meta",{"property":"og:description","content":"相关信息 按 Alt + Enter 切换全屏 1、 请检查 [修复]【 重置游戏窗口的位置和大小 】.bat 或 th123\\\\modules\\\\WindowResizer\\\\WindowResizer.ini ,建议使用 一倍 两倍 三倍 的整数倍窗口缩放，而不要手动拖拉窗口，否则不规则倍数缩放可能会导致游戏内文字或图片显示模糊等问题 2、 可以试试右键..."}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:image","content":"https://bu.dusays.com/2024/10/26/671cac0b857ea.webp =900x"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2024-10-26T09:02:29.000Z"}],["meta",{"property":"article:author","content":"三回転Tstar"}],["meta",{"property":"article:published_time","content":"2024-07-09T00:00:00.000Z"}],["meta",{"property":"article:modified_time","content":"2024-10-26T09:02:29.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"感觉游戏画面模糊\\",\\"image\\":[\\"https://bu.dusays.com/2024/10/26/671cac0b857ea.webp =900x\\"],\\"datePublished\\":\\"2024-07-09T00:00:00.000Z\\",\\"dateModified\\":\\"2024-10-26T09:02:29.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"三回転Tstar\\"}]}"]]},"headers":[],"git":{"createdTime":1707458087000,"updatedTime":1729933349000,"contributors":[{"name":"Tstar00","email":"1434716883@qq.com","commits":4}]},"readingTime":{"minutes":1.6,"words":480},"localizedDate":"2024年7月9日","autoDesc":true,"excerpt":"<div class=\\"hint-container info\\">\\n<p class=\\"hint-container-title\\">相关信息</p>\\n<ul>\\n<li>按 Alt + Enter 切换全屏</li>\\n</ul>\\n<p>1、 <strong>请检查 <code>[修复]【 重置游戏窗口的位置和大小 】.bat</code> 或 <code>th123\\\\modules\\\\WindowResizer\\\\WindowResizer.ini</code> ,建议使用 一倍 两倍 三倍 的整数倍窗口缩放，而不要手动拖拉窗口，否则不规则倍数缩放可能会导致游戏内文字或图片显示模糊等问题</strong></p>\\n<p>2、 可以试试右键 <code>th123.exe</code>，选择属性，兼容性，勾选 “高DPI缩放替代”，选择 “应用程序”<br>\\n<strong>不要选择“系统(增强)”，会导致游戏打不开</strong></p>\\n<figure><img src=\\"https://bu.dusays.com/2024/10/26/671cac0b857ea.webp\\" alt=\\"可以试试勾选“高DPI缩放替代”，选择“应用程序”\\" width=\\"900\\" tabindex=\\"0\\" loading=\\"lazy\\"><figcaption>可以试试勾选“高DPI缩放替代”，选择“应用程序”</figcaption></figure>\\n<p>3、 游戏本身就是 640*480 的分辨率，游戏本身美术素材就是像素画 ㄟ( ▔, ▔ )ㄏ<br>\\n同时也可能你的显示屏太大，放大倍数过大当然就糊了</p>\\n</div>"}');export{m as comp,u as data};
