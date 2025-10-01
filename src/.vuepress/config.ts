281006fd00e8c749f9f2e3e2e20f75a7851d5e3f
Hagb (Junyu Guo 郭俊余) <hagb@hagb.name>
2025-10-01 10:17:10 +0800

    Send url and title to parent window
    Redo #90

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -29,6 +29,47 @@ export default defineUserConfig({
     });
   },
 
+  head: [
+	  ['script', {}, `(function() {
+            if (typeof ogReplaceState != "undefined")
+              return;
+            function send(msg1, msg2) {
+              console.log("send", msg1, msg2);
+              window.parent.postMessage([msg1,msg2], "*");
+            }
+            function sendtitle() {
+              send("title", document.title);
+            }
+            var observer = new MutationObserver(function(list) {
+              for (const i of list)
+                for (const e of i.addedNodes)
+                  if (e.tagName == "TITLE")
+                    sendtitle();
+            });
+            document.head.addEventListener("DOMNodeInserted", function(event) {
+              if (event.target.tagName == "TITLE")
+                sendtitle();
+            })
+            var config = {childList: true}
+            observer.observe(document.head, config);
+          
+            function sendpath() {
+              send("path", window.location.href.substr(window.location.origin.length));
+            };
+            // https://stackoverflow.com/a/56760883
+            ogReplaceState = history.replaceState;
+            history.replaceState = function() {
+              ogReplaceState.apply(history, arguments);
+              sendpath();
+            }
+            ogPushState = history.pushState;
+            history.pushState = function() {
+              ogPushState.apply(history, arguments);
+              sendpath();
+            }
+            sendpath();
+          })();`]
+  ],
 });
 
 
